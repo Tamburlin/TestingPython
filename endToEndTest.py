a35d179d26a3a5c78851c5e34356cdaf4a2fca42
Jeremi Jagiello <jagiello.jeremi@gmail.com>
2023-04-20 15:36:06 +0200

    20.04

--- endToEndTest.py
+++ endToEndTest.py
@@ -19,3 +19,17 @@ cards = driver.find_elements(By.XPATH, "//div[@class='card h-100']")
 for card in cards:
     cardName = card.find_element(By.XPATH, "div/h4/a").text
     if cardName == "Blackberry":
+        card.find_element(By.XPATH, "div/button").click()
+
+driver.find_element(By.CSS_SELECTOR, "a[class*='btn-primary'").click()
+driver.find_element(By.XPATH, "//button[@class='btn btn-success']").click()
+driver.find_element(By.ID, "country").send_keys("ind")
+wait = WebDriverWait(driver, 10)
+wait.until(expected_conditions.presence_of_element_located((By.LINK_TEXT, "India")))
+driver.find_element(By.LINK_TEXT, "India").click()
+driver.find_element(By.XPATH, "//div[@class='checkbox checkbox-primary']").click()
+driver.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
+
+alertText = driver.find_element(By.CSS_SELECTOR, ".alert.alert-success.alert-dismissible").text
+assert "Success! Thank you" in alertText
+
