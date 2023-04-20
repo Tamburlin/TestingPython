9502545c1c177166abb89ddc7b099ae13f552774
Jeremi Jagiello <jagiello.jeremi@gmail.com>
2023-04-20 19:25:19 +0200

    pytest1

---
+++ Pytest/test_demo1.py
@@ -0,0 +1,7 @@
+#  pytest file name should begin with test_
+#  pytest test should be written in functions - it is standard, don't ask
+#  test function also should begin with test_
+
+#  to run pytest properly -> edit configurations -> + -> python test -> pytest
+def test_firstProgram():
+    print("Bye")
